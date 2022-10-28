6dabe537bf0566cb26d72748f01b2ef3ace308aa
Peter070757 <petergroeneveld@home.nl>
2022-10-28 13:10:23 +0200

    make some code and application folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import Pages from "./pages/Pages";
 
-
+import {GiForkKnifeSpoon} from "react-icons/gi";
+import StyledLogo from "./application/Logo/LogoApp";
+import StyledNav from "./application/Navigation/NavApp";
+//import Footer from './pages/Footer';
 
 function App() {
-  return (
-    <div className="App">
-<h1>Hello</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+
+            <StyledNav>
+                <GiForkKnifeSpoon/>
+                <StyledLogo to={"/"}>Happy Cooking Today</StyledLogo>
+            </StyledNav>
+
+            <Pages/>
+
+
+        </div>
+    );
 }
 
 export default App;
