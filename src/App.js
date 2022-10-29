4beac31d9b413dec4f62183f06685206551d4f29
Peter070757 <97753972+Peter070757@users.noreply.github.com>
2022-10-29 13:36:25 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import Pages from "./pages/Pages";
+import Category from "./components/Category/Category";
+import Search from "./components/Search/Search";
+import {GiForkKnifeSpoon} from "react-icons/gi";
+import StyledLogo from "./application/Logo/LogoApp";
+import StyledNav from "./application/Navigation/NavApp";
+import Footer from './pages/Footer';
+
+function App() {
+    return (
+        <div className="App">
+
+                <StyledNav>
+                    <GiForkKnifeSpoon/>
+                    <StyledLogo to={"/"}>Happy Cooking Today</StyledLogo>
+                </StyledNav>
+                <Search/>
+                <Category/>
+                <Pages/>
+                <Footer/>
+
+        </div>
+    );
+}
+
+export default App;
